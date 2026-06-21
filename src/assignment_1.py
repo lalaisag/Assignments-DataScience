b03236a9e0efdc75aad2fa18419194d47bf94d50
lalaisag <lala.isag.m@gmail.com>
2026-06-21 20:27:51 +0200

    Update assignment_1.py Lala.

--- src/assignment_1.py
+++ src/assignment_1.py
@@ -6,25 +6,25 @@ def load_data(file_path: str) -> pd.DataFrame:
     Load the CSV file using pandas.
     Return the loaded DataFrame.
     """
-    # TODO: Implement this function
-    pass
+    return pd.read_csv(file_path)
 
 def clean_age(df: pd.DataFrame) -> pd.DataFrame:
     """
     Handle missing values in the 'Age' column by filling them with the median age.
     Should return the modified DataFrame.
     """
-    # TODO: Implement this function
-    pass
+    df["Age"] = df["Age"].fillna(df["Age"].median())
+    return df
 
-def convert_types(df:pd.DataFrame) -> pd.DataFrame:
+def convert_types(df: pd.DataFrame) -> pd.DataFrame:
     """
     Convert 'CryoSleep' and 'VIP' columns to boolean types.
     Missing values in these columns should be treated as False before conversion.
     Should return the modified DataFrame.
     """
-    # TODO: Implement this function
-    pass
+    df["CryoSleep"] = df["CryoSleep"].fillna(False).astype(bool)
+    df["VIP"] = df["VIP"].fillna(False).astype(bool)
+    return df
 
 if __name__ == "__main__":
     # This block allows students to test their code locally
